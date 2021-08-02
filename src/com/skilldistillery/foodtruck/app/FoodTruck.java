eb25d4feccdbdf4647dad3a607297c43f08019b2
Jacob Tweedy <jttweedy11@gmail.com>
2021-08-01 20:46:22 -0500

    Push updates

--- src/com/skilldistillery/foodtruck/app/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruck.java
@@ -1,5 +1,69 @@
 package com.skilldistillery.foodtruck.app;
 
 public class FoodTruck {
+	private int id;
+	private String name;
+	private String foodType;
+	private double rating;
+	private static int countFoodTrucks = 0;
+	
+	public FoodTruck() {
+	}
+	
+	public FoodTruck(String pname, String pfoodType, double rating) {
+		name = pname;
+		foodType = pfoodType;
+		rating = this.rating;
+		countFoodTrucks++;
+		id = countFoodTrucks;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("FoodTruck [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", foodType=");
+		builder.append(foodType);
+		builder.append(", rating=");
+		builder.append(rating);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
 }
--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,10 +1,14 @@
 package com.skilldistillery.foodtruck.app;
-
+import java.util.Scanner;
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		FoodTruckApp tApp = new FoodTruckApp();
+		System.out.println("*********************Welcome to the Food Truck App.*************************");
+		System.out.println("**In This app, you will enter 5 of your favorate Food Trucks and rate them!*");
+		System.out.println("**You will enter the trucks name, food type, and your rating of the truck.**");
+		System.out.println("****************************************************************************");
 	}
-
 }
